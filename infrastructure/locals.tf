fce6c184e5dffc6f2eaf06d941d56ea73ca64ab0
tsrlearning <info@tsrlearning.io>
2024-06-25 02:37:32 -0500

    ghrunner install

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -17,9 +17,6 @@ locals {
       public_key     = var.public_key
       custom_data    = local.custom_data_vm_1
       vars = {
-        # RUNNER_URL = "https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz"
-        # RUNNER_SHA = "9e883d210df8c6028aff475475a457d380353f9d01877d51cc01a17b2a91161d"
-        # RUNNER_TAR = "./actions-runner-linux-x64-2.317.0.tar.gz"
         GH_PAT_TOKEN      = var.GH_PAT_TOKEN
       }
     }
